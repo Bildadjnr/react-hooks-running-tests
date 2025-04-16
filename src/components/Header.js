1dc72b9625aa2cd393ed1058fdc3bf137937cdbf
Bildad Ereggae <bildadereggae@gmail.com>
2025-04-16 09:38:22 +0300

    reat-hook running test lab done

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,8 +5,9 @@ function Header(props) {
         <h1>
           {/* uncomment the line below to pass the test */}
           {/* hello from the Header! */}
+          hello from the Header!
         </h1>
-      )
+      );
 }
 
 export default Header;
